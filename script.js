872298cedb1f11ffbf295bd0d8e8c619d4b0edf5
R3ndrex <yacholohchtoli@gmail.com>
2024-09-30 10:54:42 +0300

    Create error message when passwords dont match

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const password = document.querySelector("[id='pass']");
 const confirmPassword = document.querySelector("[id='confirm_pass']");
-const message = document.querySelector("pass-contrainer>p>span");
+const message = document.querySelector(".pass-error"); // first pass error span
 
 function validatePassword() {
-    if (password.value === confirmPassword.value) {
-        confirmPassword.setCustomValidity("");
-        message.textContent = "";
-    } else {
-        confirmPassword.setCustomValidity("Passwords do not match");
-        message.textContent = "Passwords do not match";
+    if (
+        password.value === confirmPassword.value ||
+        (password.value === "" && confirmPassword.value !== "") ||
+        (password.value !== "" && confirmPassword.value === "")
+    ) {
+        return (message.textContent = "");
     }
+    return (message.textContent = "Passwords do not match");
 }
 password.addEventListener("input", validatePassword);
 confirmPassword.addEventListener("input", validatePassword);
